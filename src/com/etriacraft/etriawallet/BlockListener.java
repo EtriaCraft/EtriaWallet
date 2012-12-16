d82b32ca1e3a5bc16bd59f0b0909681b44028d61
Shawn <MistPhizzle@Gmail.com>
2012-12-15 22:09:18 -0500

    Actual Signs.
    Signs work now (instead of the weird error they were getting :p

--- src/com/etriacraft/etriawallet/BlockListener.java
+++ src/com/etriacraft/etriawallet/BlockListener.java
@@ -9,6 +9,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -38,47 +39,93 @@ public class BlockListener implements Listener {
 			}
 		}
 	}
+	
 	@EventHandler
-	public void onPlayerInteract(PlayerInteractEvent e) {
-		Player player = e.getPlayer();
-		Block block = e.getClickedBlock();
-		BlockState state = block.getState();
-		if (state instanceof Sign) {
-			Sign sign = (Sign) state;
-			String signline1 = sign.getLine(0);
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		Block block = event.getClickedBlock();
+		if (event.getAction() != Action.LEFT_CLICK_BLOCK && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+		
+		if (block.getState() instanceof Sign) {
+			Sign s = (Sign) event.getClickedBlock().getState();
+			String signline1 = s.getLine(0);
 			if (signline1.equalsIgnoreCase("[Premium]")
-					&& e.getAction().equals(Action.RIGHT_CLICK_BLOCK)
-					&& block.getType() == Material.WALL_SIGN
-					&& player.hasPermission("etriawallet.sign.use")) {
-				String purchasedPack = sign.getLine(1);
-				Double price = plugin.getConfig().getDouble("packages." + purchasedPack + ".price");
-				String username = player.getName();
-				ResultSet playerBalance1 = DBConnection.query("SELECT balance FROM wallet_players WHERE player = '" + username + "';", false);
-				try {
-					while (playerBalance1.next()) {
-						Double newbalance = playerBalance1.getDouble("balance");
-
-						if (!(newbalance >= price)) {
-							player.sendMessage("§cYou don't have enough for this package.");
-						} else if (newbalance >= price) {
-							DBConnection.query("UPDATE wallet_players SET balance = balance = " + price + " WHERE player = '" + username + "';", true);
-							List<String> commands = plugin.getConfig().getStringList("packages." + purchasedPack + ".commands");
-							for (String cmd : commands) {
-								plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), cmd.replace("%player", username));
+				&& event.getAction().equals(Action.RIGHT_CLICK_BLOCK)
+				&& block.getType() == Material.WALL_SIGN
+				&& player.hasPermission("etriawallet.sign.use")) {
+					String purchasedPack = s.getLine(1);
+					Double price = plugin.getConfig().getDouble("packages." + purchasedPack + ".price");
+					String username = player.getName();
+					ResultSet playerBalance1 = DBConnection.query("SELECT balance FROM wallet_players WHERE player = '" + username + "';", false);
+					try {
+						while (playerBalance1.next()) {
+							Double newbalance = playerBalance1.getDouble("balance");
+							
+							if (!(newbalance >= price)) {
+								player.sendMessage("§cYou don't have enough Credit for this package.");
+							} else if (newbalance >= price) {
+								DBConnection.query("UPDATE wallet_players SET balance = balance - " + price + " WHERE player = '" + username + "';", true);
+								List<String> commands = plugin.getConfig().getStringList("packages." + purchasedPack + ".commands");
+								for (String cmd : commands) {
+									plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), cmd.replace("%player", username));
+								}
+								player.sendMessage("§aYou just purchased §3" + purchasedPack + "§a for §3" + price + "§a.");
+								player.sendMessage("§aYour wallet balance has been updated.");
+								player.sendMessage("§aTransaction completed.");
+								player.sendMessage("§a§lIf you purchased a kit, you must relog for ALL contents to show.");
 							}
-							player.sendMessage("§aWe are adding §3" + purchasedPack + "§a to your account.");
-							player.sendMessage("§aSubtracted §3" + price + "0§a from your account.");
-							player.sendMessage("§aPurchase successfully executed!");
 						}
+						event.setUseItemInHand(Result.DENY);
+						event.setUseInteractedBlock(Result.DENY);
+					} catch (SQLException ex) {
+						ex.printStackTrace();
+						player.sendMessage("§4Something went wrong with your transaction and it was not completed");
+						player.sendMessage("§4Please contact an Administrator or file a ticket.");
 					}
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-					player.sendMessage("Something went wrong with your transaction. Contact an Admin");
 				}
-			}
-			else if (block.getType() == Material.SIGN_POST) {
-				return;
-			}
 		}
 	}
+//	@EventHandler
+//	public void onPlayerInteract(PlayerInteractEvent e) {
+//		Player player = e.getPlayer();
+//		Block block = e.getClickedBlock();
+//		BlockState state = block.getState();
+//		if (state instanceof Sign) {
+//			Sign sign = (Sign) state;
+//			String signline1 = sign.getLine(0);
+//			if (signline1.equalsIgnoreCase("[Premium]")
+//					&& e.getAction().equals(Action.RIGHT_CLICK_BLOCK)
+//					&& block.getType() == Material.WALL_SIGN
+//					&& player.hasPermission("etriawallet.sign.use")) {
+//				String purchasedPack = sign.getLine(1);
+//				Double price = plugin.getConfig().getDouble("packages." + purchasedPack + ".price");
+//				String username = player.getName();
+//				ResultSet playerBalance1 = DBConnection.query("SELECT balance FROM wallet_players WHERE player = '" + username + "';", false);
+//				try {
+//					while (playerBalance1.next()) {
+//						Double newbalance = playerBalance1.getDouble("balance");
+//
+//						if (!(newbalance >= price)) {
+//							player.sendMessage("§cYou don't have enough for this package.");
+//						} else if (newbalance >= price) {
+//							DBConnection.query("UPDATE wallet_players SET balance = balance = " + price + " WHERE player = '" + username + "';", true);
+//							List<String> commands = plugin.getConfig().getStringList("packages." + purchasedPack + ".commands");
+//							for (String cmd : commands) {
+//								plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), cmd.replace("%player", username));
+//							}
+//							player.sendMessage("§aWe are adding §3" + purchasedPack + "§a to your account.");
+//							player.sendMessage("§aSubtracted §3" + price + "0§a from your account.");
+//							player.sendMessage("§aPurchase successfully executed!");
+//						}
+//					}
+//				} catch (SQLException e1) {
+//					e1.printStackTrace();
+//					player.sendMessage("Something went wrong with your transaction. Contact an Admin");
+//				}
+//			}
+//			else if (block.getType() == Material.SIGN_POST) {
+//				return;
+//			}
+//		}
+//	}
 }
